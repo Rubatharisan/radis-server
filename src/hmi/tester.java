ff5202b02019384306fe8d17e390f0b7bba6dd6c
Andreas Kastrup <akast11@student.sdu.dk>
2013-11-29 13:48:48 +0100

    Indsat functional real-time opdaterende graf

--- src/hmi/tester.java
+++ src/hmi/tester.java
@@ -1,13 +1,72 @@
 package hmi;
 
+import java.util.Random;
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+import components.Temp_sim;
+
 public class tester {
+    static TimeSeries ts = new TimeSeries("data", Millisecond.class);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws InterruptedException {
+        gen myGen = new gen();
+        new Thread(myGen).start();
 
-	}
+        TimeSeriesCollection dataset = new TimeSeriesCollection(ts);
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+            "Temperature Component",
+            "Time",
+            "Temperature",
+            dataset,
+            true,
+            true,
+            false
+        );
+        
+        final XYPlot plot = chart.getXYPlot();
+        XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer)plot.getRenderer();
+        renderer.setBaseShapesVisible(true);
+        renderer.setBaseShapesFilled(true);
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setFixedAutoRange(60000.0);
+
+        JFrame frame = new JFrame("GraphTest");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ChartPanel label = new ChartPanel(chart);
+        frame.getContentPane().add(label);
+        //Suppose I add combo boxes and buttons here later
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    static class gen implements Runnable {
+
+        public void run() {
+        	Temp_sim temp = new Temp_sim();
+      		Thread temperature_thread = new Thread(temp);
+      		temp.setTemperature(30);
+      		temperature_thread.start();
+            while(true) {
+    
+                ts.addOrUpdate(new Millisecond(), temp.getTemperature());
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
 
 }
